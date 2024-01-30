7c45c6af81a92052bc8acd76ac703ce91e9c8c7f
Fabián Karaben <fabiankaraben@gmail.com>
2024-01-30 10:29:14 -0300

    Adds two new empty package scaffold

---
+++ html_to_markdown/example/html_to_markdown_example.dart
@@ -0,0 +1,6 @@
+import 'package:html_to_markdown/html_to_markdown.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ website_downloader/example/website_downloader_example.dart
@@ -0,0 +1,6 @@
+import 'package:website_downloader/website_downloader.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
